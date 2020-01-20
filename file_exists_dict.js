921219bf4dff84ee036f6dfc41c0aa9db00135d6
kentaro.yamauchi <kentaro.yamauchi@eure.jp>
2020-01-20 13:54:06 +0900

    test

---
+++ file_exists_dict.js
@@ -0,0 +1,71 @@
+const pages_related_gtm = [
+  {
+    description: 'All_Pageview_YDNYSS',
+    paths: [
+      '/dir1',
+      '/dir1/file1',
+    ],
+  },
+  {
+    description: 'All_Transition_YDNYSS',
+    paths: [
+      '/file2',
+      // '/file2/yama',
+    ],
+  },
+]
+
+// const pages_related_gtm = [
+//   {
+//     description: 'All_Pageview_YDNYSS',
+//     paths: [
+//       '/signup/email/complete',
+//       '/signup/signup/payment',
+//     ],
+//   },
+//   {
+//     description: 'All_Transition_YDNYSS',
+//     paths: [
+//       '/signup/email/complete',
+//       '/signup/signup/payment',
+//     ],
+//   },
+//   {
+//     description: 'Form_Pageview',
+//     paths: [
+//       '/signup/email/complete',
+//     ],
+//   },
+//   {
+//     description: 'Form_Transition',
+//     paths: [
+//       '/signup/email/complete',
+//     ],
+//   },
+//   {
+//     description: 'Reg_Pageview',
+//     paths: [
+//       '/signup/signup/payment',
+//     ],
+//   },
+//   {
+//     description: 'Reg_Transition',
+//     paths: [
+//       '/signup/payment',
+//     ],
+//   },
+//   {
+//     description: 'Sub_Pageview',
+//     paths: [
+//       '/signup/payment/complete',
+//     ],
+//   },
+//   {
+//     description: 'Sub_Transition',
+//     paths: [
+//       '/signup/payment/complete',
+//     ],
+//   },
+// ]
+
+module.exports = pages_related_gtm
